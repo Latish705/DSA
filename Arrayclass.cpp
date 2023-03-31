906995f1618b9944479912ffccf97e4d746bcd21
Latish 378 <104522651+Latish705@users.noreply.github.com>
2023-03-31 21:45:10 +0530

    DSA array class

--- Arrayclass.cpp
+++ Arrayclass.cpp
@@ -1,81 +1,96 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
 
 class Array
 {
-    private:
+private:
     int capacity;
     int lastindex;
-    int*ptr;
-    public:
-    void append()
-    {
-        //append will be a fuction in this class which will help to input number the last index
-    }
-    Array(int size)
-    {
-         //inside Array constructor we will make dynamic array with the help of new
-        int*ptr=NULL;
-        ptr=new int[size];
-        capacity=size;
-        lastindex=-1;
-        cout<<capacity<<endl;
-        cout<<lastindex<<endl;
+    int *ptr;
 
-    }
-    string checkArrayEmpty()
-    {
-        if(lastindex==-1)
-        return "True";
-        else
-        return "False";
-    }
+public:
+    Array(int size);
+    string checkArrayEmpty();
     void append(int num);
-
-    void insert(int index,int num)
-    {
-        ptr=new int[(lastindex+capacity)+1];
-        *(ptr+index)=num;
-    }
-    void edit()
-    {
-
-    }
-    void displayArray()
+    void insert(int element, int index);
+    void edit(int element,int index);
+    void displayArray();
+};
+void Array::displayArray()
     {
         int i;
-        for(i=0;i<=lastindex;i++)
+        for (i = 0; i <= lastindex; i++)
         {
-            cout<<*(ptr+i);
+            cout << *(ptr + i);
+        }
+        cout<<endl;
+    }
+void Array::edit(int index,int num)
+{
+
+}
+void Array::insert(int index, int num)
+    {
+        if(lastindex==capacity-1)
+        {
+            cout<<"Array is already full"<<endl;
+        }
+        else if(index<0 || index>=lastindex+1)
+        {
+            cout<<"Invalid Index"<<endl;
+        }
+        else
+        {
+            int i;
+            for(i=lastindex;i>=index;i--)
+            {
+                ptr[i+1]=ptr[i];
+            }
+            ptr[index]=num;
+            lastindex++;
         }
     }
-};
+Array::Array(int size)
+{
+        // inside Array constructor we will make dynamic array with the help of new
+        ptr = new int[size];
+        capacity = size;
+        lastindex = -1;
+        cout << capacity << endl;
+        cout << lastindex << endl;
+    }
+
+string Array::checkArrayEmpty()
+{
+    if (lastindex == -1)
+        return "True";
+    else
+        return "False";
+}
 
 void Array::append(int num)
 {
-        if(lastindex==capacity-1)
-        {
-        cout<<endl;
-        cout<<"Error Array is already full";
-        }
-        else
-        {
-            lastindex++;
-            ptr[lastindex]=num;
-
-
-        }
-
-
-
+    if (lastindex == capacity - 1)
+    {
+        cout << endl;
+        cout << "Error Array is already full";
     }
+    else
+    {
+        lastindex++;
+        ptr[lastindex] = num;
+    }
+}
 
 int main()
 {
     Array a(5);
-    cout<<a.checkArrayEmpty();
+    cout << a.checkArrayEmpty();
     a.append(5);
+    cout << endl;
+    a.append(6);
+    a.displayArray();
+    a.insert(1,9);
     a.displayArray();
     return 0;
 }

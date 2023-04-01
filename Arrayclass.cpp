51c728478fbe2ae222803227800920de54ef8186
latish windows <latishadwani376@gmail.com>
2023-04-01 10:32:01 +0530

    Array class v1 from windows

--- Arrayclass.cpp
+++ Arrayclass.cpp
@@ -1,81 +1,166 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
 
 class Array
 {
-    private:
+private:
     int capacity;
     int lastindex;
-    int*ptr=NULL;
-    public:
-    void append()
-    {
-        //append will be a fuction in this class which will help to input number the last index
-    }
-    Array(int size)
-    {
-         //inside Array constructor we will make dynamic array with the help of new
-        int*ptr=NULL;
-        ptr=new int[size];
-        capacity=size;
-        lastindex=-1;
-        cout<<capacity<<endl;
-        cout<<lastindex<<endl;
+    int *ptr;
 
-    }
-    string checkArrayEmpty()
-    {
-        if(lastindex==-1)
-        return "True";
-        else
-        return "False";
-    }
+public:
+    Array(int size);
+    string checkArrayEmpty();
     void append(int num);
+    void insert(int element, int index);
+    void edit(int element,int index);
+    void displayArray();
+    void deleteElement(int index);
+    int getEle(int index);
+    bool isArrayFull();
+    void countEle();
 
-    void insert(int index,int num)
-    {
-        ptr=new int[(lastindex+capacity)+1];
-        *(ptr+index)=num;
-    }
-    void edit()
-    {
+    ~Array();
+};
 
+//version 2 in this version we are going to add sorting,greatest element ,smallest element,search ele,sum of all element,average of all ele,rotate,
+
+//version 1
+Array::~Array()
+{
+    delete[]ptr;
+}
+void Array::countEle()
+{
+    cout<<"Number element present is "<<lastindex+1<<endl;
+}
+bool Array::isArrayFull()
+{
+    //this is how sir taught to make this function
+    return lastindex==capacity-1;      
+}
+int Array::getEle(int index)
+{
+    if(index==-1||index>lastindex)
+    {
+        cout<<"Invalid Index"<<endl;
     }
-    void displayArray()
+    else
+    {
+        return ptr[index];
+    }
+}
+void Array::deleteElement(int index)
+{
+    if(lastindex==-1)
+    {
+        cout<<"Array is Empty"<<endl;
+    }
+    else if(index==-1||index>lastindex)
+    {
+        cout<<"Invalid Index"<<endl;
+    }
+    else
     {
         int i;
-        for(i=0;i<=lastindex;i++)
+        for(i=index;i<lastindex;i++)
         {
-            cout<<*(ptr+i);
+            ptr[i]=ptr[i+1];
+        }
+        lastindex--;
+    }
+}
+void Array::displayArray()
+    {
+        int i;
+        for (i = 0; i <= lastindex; i++)
+        {
+            cout << *(ptr + i)<<" ";
+        }
+        cout<<endl;
+    }
+void Array::edit(int index,int num)
+{
+    ptr[index]=num;
+}
+void Array::insert(int index, int num)
+    {
+        if(lastindex==capacity-1)
+        {
+            cout<<"Array is already full"<<endl;
+        }
+        else if(index<0 || index>=lastindex+1)
+        {
+            cout<<"Invalid Index"<<endl;
+        }
+        else
+        {
+            int i;
+            for(i=lastindex;i>=index;i--)
+            {
+                ptr[i+1]=ptr[i];
+            }
+            ptr[index]=num;
+            lastindex++;
         }
     }
-};
+Array::Array(int size)
+{
+        // inside Array constructor we will make dynamic array with the help of new
+        ptr = new int[size];
+        capacity = size;
+        lastindex = -1;
+        cout << capacity << endl;
+        cout << lastindex << endl;
+    }
+
+string Array::checkArrayEmpty()
+{
+    if (lastindex == -1)
+        return "True";
+    else
+        return "False";
+}
 
 void Array::append(int num)
 {
-        if(lastindex==capacity-1)
-        {
-        cout<<endl;
-        cout<<"Error Array is already full";
-        }
-        else
-        {
-            lastindex++;
-            ptr[lastindex]=num;
-
-
-        }
-
-
-
+    if (lastindex == capacity - 1)
+    {
+        cout << endl;
+        cout << "Error Array is already full";
     }
+    else
+    {
+        lastindex++;
+        ptr[lastindex] = num;
+    }
+}
 
 int main()
 {
-    Array a(5);
-    cout<<a.checkArrayEmpty();
-    a.append(5);
+    Array a(11);
+    cout << a.checkArrayEmpty();
+    a.append(11);
+    cout << endl;
+    a.append(22);
+    a.append(33);
+    a.append(44);
     a.displayArray();
+    a.insert(1,9);
+    a.displayArray();
+    a.edit(3,32);
+    a.displayArray();
+    a.append(55);
+    a.append(67);
+    a.append(78);
+    a.displayArray();
+    a.deleteElement(5);
+    a.displayArray();
+    cout<<a.getEle(4)<<endl;
+    if(a.isArrayFull())
+    cout<<"Array is full"<<endl;
+    else
+    cout<<"Array is not full"<<endl;
+    a.countEle();
     return 0;
 }

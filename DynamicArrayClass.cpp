041d64a06b72feccb437eff8c658fa724fae8381
latish windows <latishadwani376@gmail.com>
2023-04-02 14:03:55 +0530

    Dynamic Array class DSA

--- DynamicArrayClass.cpp
+++ DynamicArrayClass.cpp
@@ -2,10 +2,131 @@
 using namespace std;
 
 
-class vector
+class DynArray
 {
-
+    //find
+    private:
+        int capacity;
+        int lastindex;
+        int *ptr;
+    public:
+        DynArray(int size);
+        void doubleArray();
+        void halfArray();
+        int Ccapacity();
+        bool checkArrayempty();
+        void append(int element);
+        void insert(int index,int element);
+        void edit(int index,int element);
+        void DynArray::deleteElement(int index);
 };
+void DynArray::deleteElement(int index)
+{
+    if (lastindex == -1)
+    {
+        halfArray();
+          int i;
+        for (i = index; i < lastindex; i++)
+        {
+            ptr[i] = ptr[i + 1];
+        }
+        lastindex--;
+
+    }
+    else if (index == -1 || index > lastindex)
+    {
+        cout << "Invalid Index" << endl;
+    }
+    else
+    {
+        int i;
+        for (i = index; i < lastindex; i++)
+        {
+            ptr[i] = ptr[i + 1];
+        }
+        lastindex--;
+    }
+}
+void DynArray::edit(int index,int element)
+{
+    ptr[index] = element;
+}
+void DynArray::insert(int index, int element)
+{
+    if (lastindex == capacity - 1)
+    {
+        doubleArray();
+        int i;
+        for (i = lastindex; i >= index; i--)
+        {
+            ptr[i + 1] = ptr[i];
+        }
+        ptr[index] =element;
+        lastindex++;
+    }
+    else if (index < 0 || index >= lastindex + 1)
+    {
+        cout << "Invalid Index" << endl;
+    }
+    else
+    {
+        int i;
+        for (i = lastindex; i >= index; i--)
+        {
+            ptr[i + 1] = ptr[i];
+        }
+        ptr[index] = element;
+        lastindex++;
+    }
+}
+void DynArray::append(int element)
+{
+    if (lastindex == capacity - 1)
+    {
+        doubleArray();
+        
+    }
+    else
+    {
+        lastindex++;
+        ptr[lastindex] = element;
+    }
+}
+
+bool DynArray::checkArrayempty()
+{
+    if (lastindex == -1)
+        return 1;
+    else
+        return 0;
+}
+DynArray::DynArray(int size)
+{
+    ptr=new int[size];
+    lastindex=-1;
+    capacity=size;
+}
+void DynArray::doubleArray()
+{
+    int *temp;
+    temp=new int[capacity*2];
+    delete[]ptr;
+    ptr=temp;
+    capacity=capacity*2;
+    
+}
+void DynArray::halfArray()
+{
+    int *temp;
+    temp=new int[capacity/2];
+    delete[]ptr;
+    ptr=temp;
+    capacity=capacity/2;
+}
+int DynArray::Ccapacity()
+{
+    return capacity;
+}
 
 
 

64262beb99eb4b545f97c8d915380d22dd633b51
Latish 378 <104522651+Latish705@users.noreply.github.com>
2023-08-09 18:02:01 +0530

    new version of BST

--- college/BST.cpp
+++ college/BST.cpp
@@ -17,30 +17,46 @@ class BST
     BST();
     bool isTreeEmpty();
     void insert(int data);
-    void preorderTraversing();
+    void preorderTraversing(node *root);
+    node *GetRoot();
 };
-void BST::preorderTraversing()
+node *BST::GetRoot()
 {
-    
+    return root;
 }
-void BST::insert(int data)
+void BST::preorderTraversing(node *root)
 {
     if(root)
     {
-        node *n=new node;
-        n->item=data;
+        preorderTraversing(root->left);
+        preorderTraversing(root->right);
+        cout<<root->item<<" ";
+    }
+}
+void BST::insert(int data)
+{
+    node *n=new node;
+    n->item=data;
+    if(root)
+    {
         node *t=root;
         while(t->left||t->right)
         {
             if(t->item>data)
                 t=t->right;
-            else
+            else if(t->item<data)
                 t=t->left;
+            else
+                break;
         }
         if(t->item>data)
             t->right=n;
         else
             t->left=n;
+    }
+    else
+    {
+        root=n;
     }
 }
 bool BST::isTreeEmpty()
@@ -53,6 +69,16 @@ BST::BST()
 }
 int main()
 {
-
+    BST b1;
+    b1.insert(12);
+    b1.preorderTraversing(b1.GetRoot());
+    b1.insert(11);
+    b1.insert(15);
+    // b1.insert(16);
+    // b1.insert(17);
+    // b1.insert(13);
+    // b1.insert(44);
+    // b1.insert(1);
+    b1.preorderTraversing(b1.GetRoot());
     return 0;
 }
